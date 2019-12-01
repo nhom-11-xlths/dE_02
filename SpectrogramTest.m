92e24bc8aef46e4af90c2ec984a34c4491dc7359
PhanVu <phanvu200470@gmail.com>
2019-12-01 22:54:52 +0700

    Thực Nghiệm + Kết Luận + source Code
    kết quả thực nghiệm

--- SpectrogramTest.m
+++ SpectrogramTest.m
@@ -1,15 +1,15 @@
 clear, clc, close all
 % load an audio file
 [x, fs] = audioread('aeiuo_lon_data.wav'); 
-% Tham sô ??u vào
-L = 256; % Chi?u dài c?a s?
-step = L/4; % B??c nh?y gi?a các c?a s?
-NFFT = 2048;% S? ?i?m l?y FFT
+% Tham sô dau vào
+L = 256; % Chieu dai cua so
+step = L/4; % Buoc nhay giua cac cua so
+NFFT = 2048;% So diem lay FFT
 win = hanning(L);
 [S, f, t] = stft(x,win,step,NFFT,fs);
 
 S=(S)';
-S=S/max(max(S)); 
+S=S/max(max(S)); % Chuan hoa bien do
 S = 20*log10(S);
 
 figure(1)
@@ -18,17 +18,13 @@ shading interp
 axis tight
 view(0, 90)
 set(gca, 'FontName', 'Times New Roman', 'FontSize', 14)
-xlabel('Time, s')
-ylabel('Frequency, Hz')
-title('Amplitude spectrogram of the signal')
+xlabel('Thoi gian, s')
+ylabel('Tan so, Hz')
+title('Pho bien do cua tin hieu')
 hcol = colorbar;
 set(hcol, 'FontName', 'Times New Roman', 'FontSize', 14)
-ylabel(hcol, 'Magnitude, dB')
+ylabel(hcol, 'Cuong do am, dB')
 
-%colormap(1-gray); 
-%colormap(jet); 
-%imagesc(t,flipud(f),20*log10(S),[-100 0]);
-%axis xy
 
 
 
--- stft.m
+++ stft.m
@@ -1,22 +1,22 @@
 function [S, f, t] = stft(x, win, step, NFFT, fs)
 %
 % Input:
-% x - Tín hi?u trên mi?n th?i gian
-% win - Hàm c?a s?
-% step - B??c nh?y c?a s?
-% NFFT - Sô ?i?m l?y FFT
-% fs - T?n s? l?y m?u, Hz
+% x - Tín hieu tren mien thoi gian
+% win - Ham cua so window
+% step - Buoc nhay cua so
+% NFFT - So diem lay FFT
+% fs - Tan so lay mau, Hz
 %
 % Output:
 % STFT - STFT-Ma tr?n (only unique points, time 
 %        across columns, frequency across rows)
-% f - Vector t?n s?, Hz
-% t - Vector th?i gian, s
+% f - Vector tan so, Hz
+% t - Vector thoi gian, s
 N = length(x);
 L = length(win);
-K = fix((N-L + step)/step); % Tính s? l??ng khung tín hi?u
-N2 = NFFT/2 + 1; % Tính s? ?i?m fft duy nh?t(không l?p l?i)
-S =zeros(K,N2); % Khai báo ma tr?n STFT
+K = fix((N-L + step)/step); % Tính so luong khung tin hieu
+N2 = NFFT/2 + 1; % Tinh so diem fft duy nhat(không lap lai)
+S =zeros(K,N2); % Khai bao ma tran STFT
 for k=0:K-1
     % windowing
     xw=x(1+k*step:L+k*step).*win;     
@@ -25,7 +25,7 @@ for k=0:K-1
     X1=X(1:N2)';
     S(k+1,1:N2)=X1.*conj(X1); 
 end
-% Tính Vector th?i gian và t?n s?
+% Tính Vector thoi gian và tan so
 t=(0:K-1)'*step/fs;
 f=(0:NFFT/2)'*fs/NFFT;
 end
