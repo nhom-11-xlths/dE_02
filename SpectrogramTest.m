8349f73457e276e4487c733bf2d9d8323f21db58
PhanVu <phanvu200470@gmail.com>
2019-12-01 17:18:03 +0700

    Code V2
    Code Version 2

--- SpectrogramTest.m
+++ SpectrogramTest.m
@@ -1,15 +1,34 @@
 clear, clc, close all
 % load an audio file
-[x, fs] = audioread('a.wav'); 
+[x, fs] = audioread('aeiuo_lon_data.wav'); 
 % Tham sô ??u vào
-L = 2048; % Chi?u dài c?a s?
+L = 256; % Chi?u dài c?a s?
 step = L/4; % B??c nh?y gi?a các c?a s?
-NFFT = 4096;% S? ?i?m l?y FFT
+NFFT = 2048;% S? ?i?m l?y FFT
 win = hanning(L);
 [S, f, t] = stft(x,win,step,NFFT,fs);
-S=fliplr(S)';
-S=S/max(max(S));
-colormap(1-gray);  colormap(jet);
-imagesc(t,flipud(f),20*log10(S),[-100 0]); axis xy
+
+S=(S)';
+S=S/max(max(S)); 
+S = 20*log10(S);
+
+figure(1)
+surf(t, f, S)
+shading interp
+axis tight
+view(0, 90)
+set(gca, 'FontName', 'Times New Roman', 'FontSize', 14)
+xlabel('Time, s')
+ylabel('Frequency, Hz')
+title('Amplitude spectrogram of the signal')
+hcol = colorbar;
+set(hcol, 'FontName', 'Times New Roman', 'FontSize', 14)
+ylabel(hcol, 'Magnitude, dB')
+
+%colormap(1-gray); 
+%colormap(jet); 
+%imagesc(t,flipud(f),20*log10(S),[-100 0]);
+%axis xy
+
 
 
